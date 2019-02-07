b9b6334f53485bb463c63d045fd693696daa510d
ARYAKARTHIKA <aryaip1997@gmail.com>
2019-02-07 15:27:50 +0530

    LIST

--- lists.py
+++ lists.py
@@ -1,2 +1,10 @@
-sub=["phy","che","maths",1,2,3]
+sub=["phy","che","maths",1,2]
+print(sub)
+print(sub[0])
+print(sub[0:2])
+sub[0]="Eng"
+print(sub)
+sub[0:2]=["Eng","science"]
+print(sub)
+del sub[1]
 print(sub)
