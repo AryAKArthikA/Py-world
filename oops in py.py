e24e4ee542c74db36d4a449ca096b5367d8294c0
ARYAKARTHIKA <aryaip1997@gmail.com>
2019-02-08 11:32:43 +0530

    OOPS

---
+++ oops in py.py
@@ -0,0 +1,10 @@
+class students:
+    name=""
+    age=""
+    def display(self,x,y):
+        students.name=x
+        students.age=y
+        print(students.name)
+        print(students.age)
+s= students()
+s.display("Rajesh",54)
